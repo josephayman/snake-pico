adcf0a31e0797802185bf3cc46a4466a864653ed
Joseph Ayman <josephayman@mail.com>
2023-11-07 19:39:13 +0100

    UPDATE

--- main.py
+++ main.py
@@ -2,140 +2,145 @@ from machine import Pin
 from util import LCD, color_rgb
 import random
 import utime
-    
+
 
 # 1
-# Set up button pins.
-# These are all instances of machine.Pin.
-# See docs at https://docs.micropython.org/en/latest/library/machine.Pin.html#machine.Pin
+# Set up button pins
 btn_a = Pin(15, Pin.IN, Pin.PULL_UP)
 btn_b = Pin(17, Pin.IN, Pin.PULL_UP)
 btn_x = Pin(19, Pin.IN, Pin.PULL_UP)
 btn_y = Pin(21, Pin.IN, Pin.PULL_UP)
 
 # 2
-# Set up joystick pins.
-# These are also machine.Pin instances.
+# Set up joystick pins
 joy_up = Pin(2, Pin.IN, Pin.PULL_UP)
 joy_down = Pin(18, Pin.IN, Pin.PULL_UP)
 joy_left = Pin(16, Pin.IN, Pin.PULL_UP)
 joy_right = Pin(20, Pin.IN, Pin.PULL_UP)
 
 # 3
-# Initialise LCD.
-# This is a framebuf.FrameBuffer instance. See docs at:
-# https://docs.micropython.org/en/latest/library/framebuf.html
+# Initialise LCD
 lcd = LCD()
 
 # 4
-# Create some colors.
-# color_rgb is a function from our utility library.
+# Create some colors
 BLACK = color_rgb(0, 0, 0)
-RED   = color_rgb(255, 0, 0)
+RED = color_rgb(255, 0, 0)
 GREEN = color_rgb(0, 255, 0)
-BLUE  = color_rgb(0, 0, 255)
-YELLOW = color_rgb(255, 255, 0)
+BLUE = color_rgb(0, 0, 255)
 WHITE = color_rgb(255, 255, 255)
 
-
 # 5
-# Size of the display in pixels.
+# Size of the display in pixels
 WIDTH = 240
 HEIGHT = 240
 
-
-# Position of snake
-snake_x = 120
-snake_y = 120
-
-box = (10, 10)
+# Function to draw a square
 
 
-# Game objects
-snake = [(120, 120), (120, 130), (120, 140)]
-snake_direction = 'up'
-snake_speed = 5
-food = (100, 60)
+def draw_square(x, y, size, color):
+    for i in range(x, x + size):
+        for j in range(y, y + size):
+            lcd.pixel(i, j, color)
+
+# Game loop
 
 
+def main_game_loop():
+    # Position of snake
+    snake_x = 120
+    snake_y = 120
 
-while True:
-    #
-    # CLEAR SCREEN AND ADD BANNER
-    #
-
-    # Fill the screen with black color.
-    lcd.fill(BLACK)
-
-    # Display a nice banner.
-    lcd.text('JOSALA @ KANTEGA', 60, 60, BLUE)
-
-
-    # Draw the snake
-    for x, y in snake:
-        lcd.pixel(x, y, WHITE)
-
-    # Draw the food
-    lcd.pixel(food[0], food[1], GREEN)
-
-    # Get the joystick input
-    if not joy_up.value():
-        snake_direction = 'up'
-    elif not joy_down.value():
-        snake_direction = 'down'
-    elif not joy_left.value():
-        snake_direction = 'left'
-    elif not joy_right.value():
-        snake_direction = 'right'
-    
-    # Move the snake
-    if snake_direction == 'up':
-        snake_y -= snake_speed
-    elif snake_direction == 'down':
-        snake_y += snake_speed
-    elif snake_direction == 'left':
-        snake_x -= snake_speed
-    elif snake_direction == 'right':
-        snake_x += snake_speed
-
-    # Check if snake is outside the screen
-    if snake_x < 0 or snake_x > WIDTH or snake_y < 0 or snake_y > HEIGHT:
+    # Game objects
+    snake = [(120, 120), (120, 130), (120, 140)]
+    snake_direction = 'up'
+    snake_speed = 5
+    food = (100, 60)
+    while True:
+        # Clear screen and add banner
         lcd.fill(BLACK)
-        lcd.text('GAME OVER', 50, 120, RED)
-        lcd.show()
-        break
+        lcd.text('JOSALA @ KANTEGA', 60, 60, BLUE)
 
+        # Draw the snake
+        for segment in snake:
+            # Snake is 10x10 pixels
+            draw_square(segment[0], segment[1], 10, WHITE)
 
-    # Check if snake has eaten the food
-    if snake_x == food[0] and snake_y == food[1]:
-        # Extend the snake
-        snake.insert(0, (snake_x, snake_y))
-        
-        # Generate new food location
-        while True:
-            new_food = (random.randint(0, WIDTH-1), random.randint(0, HEIGHT-1))
-            if new_food not in snake:
-                food = new_food
-                break
-    
-    else:
+        # Draw the food
+        draw_square(food[0], food[1], 10, GREEN)  # Food is 10x10 pixels
+
+        # Get the joystick input
+        if not joy_up.value():
+            snake_direction = 'up'
+        elif not joy_down.value():
+            snake_direction = 'down'
+        elif not joy_left.value():
+            snake_direction = 'left'
+        elif not joy_right.value():
+            snake_direction = 'right'
+
         # Move the snake
-        snake.insert(0, (snake_x, snake_y))
-        snake.pop()
-    
-    # Check for collisions with itself
-    if (snake_x, snake_y) in snake[1:]:
+        if snake_direction == 'up':
+            snake_y -= snake_speed
+        elif snake_direction == 'down':
+            snake_y += snake_speed
+        elif snake_direction == 'left':
+            snake_x -= snake_speed
+        elif snake_direction == 'right':
+            snake_x += snake_speed
+
+        # Check if snake is outside the screen
+        if snake_x < 0 or snake_x >= WIDTH or snake_y < 0 or snake_y >= HEIGHT:
+            lcd.fill(BLACK)
+            lcd.text('GAME OVER', 50, 120, RED)
+            lcd.show()
+            break
+
+        # Check if snake has eaten the food
+        if (snake_x in range(food[0], food[0] + 10)) and (snake_y in range(food[1], food[1] + 10)):
+            # Extend the snake
+            snake.insert(0, (snake_x, snake_y))
+
+            # Generate new food location
+            while True:
+                new_food_x = random.randint(0, WIDTH - 10)
+                new_food_y = random.randint(0, HEIGHT - 10)
+                new_food = (new_food_x, new_food_y)
+                if all((nx, ny) not in snake for nx in range(new_food_x, new_food_x + 10) for ny in range(new_food_y, new_food_y + 10)):
+                    food = new_food
+                    break
+        else:
+            # Move the snake
+            snake.insert(0, (snake_x, snake_y))
+            snake.pop()
+
+        # Check for collisions with itself
+        if (snake_x, snake_y) in snake[1:]:
+            lcd.fill(BLACK)
+            lcd.text('GAME OVER', 50, 120, RED)
+            lcd.show()
+            break
+
+        # Render everything to screen
+        lcd.show()
+
+        # Delay to control game speed
+        utime.sleep_ms(100)
+
+        # Check for game over and prompt for restart
+    if (snake_x < 0 or snake_x >= WIDTH or snake_y < 0 or snake_y >= HEIGHT) or ((snake_x, snake_y) in snake[1:]):
         lcd.fill(BLACK)
         lcd.text('GAME OVER', 50, 120, RED)
+        lcd.text('Press A to restart', 35, 140, WHITE)
         lcd.show()
-        break
-    
+        while True:
+            # Wait for button A to be pressed to restart
+            if not btn_a.value():
+                main_game_loop()
+            utime.sleep_ms(100)
 
-    #
-    # FINAL DRAWING
-    #
 
-    # Render everything to screen...
-    lcd.show()
-    # Delay to control game speed
-    utime.sleep_ms(100)
+print('Starting Snake game')
+main_game_loop()
+
+#
